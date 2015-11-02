f979783d55e04d31d853c4ce1d0c92f1ca2788e4
netcreative <epop.bill@gmail.com>
2015-11-02 11:18:14 -0500

    added directive to allow links in chat + removed unused files

--- js/directives.js
+++ js/directives.js
@@ -1,3 +1,20 @@
+String.prototype.linkify = function() {
+
+        // http://, https://, ftp://
+        var urlPattern = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim;
+
+        // www. sans http:// or https://
+        var pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+
+        // Email addresses
+        var emailAddressPattern = /[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim;
+
+        return this
+            .replace(urlPattern, '<a href="$&">$&</a>')
+            .replace(pseudoUrlPattern, '$1<a href="http://$2">$2</a>')
+            .replace(emailAddressPattern, '<a href="mailto:$&">$&</a>');
+};
+
 angular.module('mychat.directives', [])
 
 .directive('myMaxlength', [function() {
@@ -23,7 +40,20 @@ angular.module('mychat.directives', [])
       ngModelCtrl.$parsers.push(fromUser);
     }
   }; 
-}]);
+}])
 
+.directive('myLinks', ['$compile', '$sce', function ($compile, $sce){
+    
+    return {
+        restrict: 'AE',
+        scope: {
+          links: '='
+        },
+        template: '<span ng-bind-html="messageWithLinks">{{messageWithLinks}}</span>',
+        link: function (scope, element, attrs){
+             scope.messageWithLinks = $sce.trustAsHtml(scope.links.message.linkify());
 
+        }
+    }
 
+}])
